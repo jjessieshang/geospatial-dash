a28c72dee1b2d3a5a792352b1fc453af0aec3f07
jjessieshang <17jessies@gmail.com>
2023-06-29 13:30:48 -0700

    merging dash app code

---
+++ intro.py
@@ -0,0 +1,156 @@
+import pandas as pd
+import plotly.express as px  # (version 4.7.0 or higher)
+import plotly.graph_objects as go
+from dash import Dash, dcc, html, Input, Output, dash_table  # pip install dash (version 2.0.0 or higher) 
+from collections import OrderedDict
+from plotly.subplots import make_subplots
+
+app = Dash(__name__)
+
+# ------------------------------------------------------------------------------
+# import, clean, and process data
+df = pd.read_csv("intro_bees.csv")
+df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
+df.reset_index(inplace=True)
+print(df[:5]),
+
+#sample table data
+data = OrderedDict(
+[
+    ("Date", ["2015-01-01", "2015-10-24", "2016-05-10", "2017-01-10", "2018-05-10", "2018-08-15"]),
+    ("Region", ["Montreal", "Toronto", "New York City", "Miami", "San Francisco", "London"]),
+    ("Temperature", [1, -20, 3.512, 4, 10423, -441.2]),
+    ("Humidity", [10, 20, 30, 40, 50, 60]),
+    ("Pressure", [2, 10924, 3912, -10, 3591.2, 15]),
+]
+),
+
+labels = ["US", "China", "European Union", "Russian Federation", "Brazil", "India",
+          "Rest of World"]
+
+# Create subplots: use 'domain' type for Pie subplot
+fig = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]])
+fig.add_trace(go.Pie(labels=labels, values=[16, 15, 12, 6, 5, 4, 42], name="GHG Emissions"),
+              1, 1)
+fig.add_trace(go.Pie(labels=labels, values=[27, 11, 25, 8, 1, 3, 25], name="CO2 Emissions"),
+              1, 2)
+
+# Use `hole` to create a donut-like pie chart
+fig.update_traces(hole=.4, hoverinfo="label+percent+name")
+
+fig.update_layout(
+    title_text="Global Emissions 1990-2011",
+    # Add annotations in the center of the donut pies.
+    annotations=[dict(text='GHG', x=0.18, y=0.5, font_size=20, showarrow=False),
+                 dict(text='CO2', x=0.82, y=0.5, font_size=20, showarrow=False)])
+
+# ------------------------------------------------------------------------------
+# app layout: contains dash components and html
+app.layout = html.Div(className="main-content", children=[
+    html.H1(className="dash-title", children=["Healthcare Costing Across British Columbia"]),
+
+    html.Div(className="main-row", children=[
+        html.Div(className="column1", children=[
+            html.H3(className="section-title", 
+                    children=["Choose a health authority and age group from the list below to view costs"]),
+            # dropdown menus
+            html.Label(className="select-label", children="Select a Health Authority"),
+            dcc.Dropdown(className="slct_age", id="slct_year",
+            options=[
+                {"label": "2015", "value": 2015},
+                {"label": "2016", "value": 2016},
+                {"label": "2017", "value": 2017},
+                {"label": "2018", "value": 2018}],
+            multi=False,
+            value=2015,
+            ),
+            html.Label(className="select-label", children="Select an Age Group"),
+            dcc.Dropdown(className="slct-age", id="slct-age",
+            options=[
+                {"label": "0-14 years", "value": 2015},
+                {"label": "0-14 years", "value": 2016},
+                {"label": "0-14 years", "value": 2017},
+                {"label": "0-14 years", "value": 2018}],
+            multi=False,
+            value=2015,
+            ),
+
+            # cost of care summary
+            html.Div(className="cost-summary", children=[
+                html.H1("Cost of Care Summary"),
+                html.Label(className="select-label", children="Method of Care Cost Breakdown"),
+                dcc.Dropdown(className="slct-cost-type", id="slct-cost-type",
+                options=[
+                    {"label": "Emergency Visit", "value": 2015},
+                    {"label": "Family Doctor", "value": 2016},
+                    {"label": "Virtual Care", "value": 2017},],
+                multi=False,
+                value=2015,
+                ),
+                html.Br(),
+                dash_table.DataTable(
+                    data=df.to_dict('records'),
+                    columns=[{'id': c, 'name': c} for c in df.columns],
+                    style_cell={'textAlign': 'left'},
+                    style_cell_conditional=[
+                        {
+                            'if': {'column_id': 'Region'},
+                            'textAlign': 'left'
+                        }
+                    ],
+                    style_table={'height': '550px', 'overflowY': 'auto'}
+                ),
+
+                html.Div(className="my_rose", children=[]),
+            ])
+        ]),
+
+        html.Div(className="column2", children=[
+
+                html.Div(id='output_container', children=[]),
+                html.Br(),
+
+                dcc.Graph(id='my_bee_map', figure={}),
+
+
+                dcc.Graph(id='my_polar1', figure=fig),
+        ]),
+    ]),
+
+])
+
+# ------------------------------------------------------------------------------
+# Connect the Plotly graphs with Dash Components
+# each callback has inputs and outputs, along with a function with arguments that realte to each input
+
+@app.callback(
+    [Output(component_id='output_container', component_property='children'),
+     Output(component_id='my_bee_map', component_property='figure')],
+    [Input(component_id='slct_year', component_property='value')]
+)
+def update_graph(option_slctd):
+    print(option_slctd)
+    print(type(option_slctd))
+
+    container = "The year chosen by user was: {}".format(option_slctd)
+
+    dff = df.copy() #always make a copy of df
+    dff = dff[dff["Year"] == option_slctd]
+    dff = dff[dff["Affected by"] == "Varroa_mites"]
+
+    # Plotly Express
+    fig = px.choropleth(
+        data_frame=dff,
+        locationmode='USA-states',
+        locations='state_code',
+        scope="usa",
+        color='Pct of Colonies Impacted',
+        hover_data=['State', 'Pct of Colonies Impacted'],
+        color_continuous_scale=px.colors.sequential.YlOrRd,
+        labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
+        # template='plotly_dark'
+    )
+    return container, fig
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app.run_server(debug=True)
