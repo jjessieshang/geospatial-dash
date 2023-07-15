708027fc7beacd4d086b2c306167899050ff97db
jjessieshang <17jessies@gmail.com>
2023-07-14 20:07:45 -0700

    simple styling

--- intro.py
+++ intro.py
@@ -15,9 +15,6 @@ df2.reset_index(inplace=True)
 df2 = df2.rename(columns={'lost_productivity': 'Lost Productivity', 'informal_caregiver': 'Informal Caregiver', 'out_of_pocket': 'Out of Pocket', 
                           'cost_value': 'Total'})
 
-df = px.data.election()
-geojson = px.data.election_geojson()
-
 
 # ------------------------------------------------------------------------------
 # app layout: contains dash components and html
@@ -28,109 +25,45 @@ app.layout = html.Div(className="main-content", children=[
         html.Div(className="column1", children=[
             html.H3(className="section-title", 
                     children=["Choose a health authority and age group from the list below to view costs"]),
+            html.P(className="section-title", 
+                children=["f type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in"]),
             # dropdown menus
-            html.Label(className="select-label", children="Select a Health Authority"),
-            dcc.Dropdown(className="slct_ha", id="slct_ha",
+            html.Label(className="select-label", children="Select the Health Authorities you would like to view"),
+            # multi select?
+            dcc.Dropdown(className="multi_slct-ha", id="multi_slct-ha",
             options=[
                 {"label": "Northern", "value": "Northern"},
                 {"label": "Interior", "value": "Interior"},
                 {"label": "Vancouver Coastal", "value": "Vancouver Coastal"},
                 {"label": "Fraser", "value": "Fraser"},
                 {"label": "Vancouver Island", "value": "Vancouver Island"}],
-            multi=False,
-            value="Vancouver Island",
+                    multi=True,
+                    placeholder="Select health authorities",
             ),
             html.Br(),
-            html.Label(className="select-label", children="Select an Age Group"),
-            dcc.Dropdown(className="slct_age", id="slct_age",
-            options=[
-                {"label": "0-14 years", "value": "0-14"},
-                {"label": "15-64 years", "value": "15-64"},
-                {"label": "65+ years", "value": "65+"}],
-            multi=False,
-            value="0-14"
-            ),
-
-            # cost of care table and dropdowns
-            html.Div(className="cost-summary", children=[
-                html.H1("Cost of Care Summary"),
-                html.Label(className="select-label", children="Method of Care"),
-                dcc.Dropdown(className="slct-service-type", id="slct-service-type",
+            html.Label(className="select-label", children="Select the comparison parameter"),
+            dcc.RadioItems(className="plot-comparator", id="plot-comparator",
                 options=[
-                    {"label": "Emergency Care", "value": "emergency"},
-                    {"label": "Family Doctor", "value": "family medicine"},
-                    {"label": "Virtual Care", "value": "virtual"},
-                    {"label": "Hospitalization", "value": "hospitalization"},],
+                    {'label': 'Age', 'value': 'age'},
+                    {'label': 'Service Type', 'value': 'service_type'},
+                ],
+            ),
+            html.Br(),
+            html.Label(className="select-label", children="Which cost category would you like to plot?"),
+            dcc.Dropdown(className="plot-basis", id="plot-basis", 
                 multi=False,
-                value="virtual",
-                ),
-                html.Br(),
-                html.Label(className="select-label", children="Canadian Triage and Acuity Scale (CTAS) Level"),
-                # dcc.Dropdown(className="slct-ctas", id="slct-ctas", 
-                # multi=False,
-                # value="1-3",
-                # ),
-                html.Br(),
-                dash_table.DataTable(
-                    id="datatable",
-                    style_table={'height': '550px', 'overflowY': 'auto'},
-                    style_cell_conditional=[
-                        {
-                            'if': {'column_id': c},
-                            'textAlign': 'left'
-                        } for c in ['Cost Category', 'Amount']
-                    ],
-                    style_data={
-                        'color': 'black',
-                        'backgroundColor': 'white'
-                    },
-                    style_data_conditional=[
-                        {
-                            'if': {'row_index': 'odd'},
-                            'backgroundColor': 'rgb(230, 230, 230)',
-                        }
-                    ],
-                    style_header={
-                        'backgroundColor': 'rgb(230, 230, 230)',
-                        'color': 'black',
-                        'fontWeight': 'bold'
-                    }
-                ),
-                html.Div(className="my_rose", children=[]),
-            ])
+            ),
         ]),
 
         html.Div(className="column2", children=[
-                # multi select?
-                dcc.Dropdown(className="multi_slct-ha", id="multi_slct-ha",
-                options=[
-                    {"label": "Northern", "value": "Northern"},
-                    {"label": "Interior", "value": "Interior"},
-                    {"label": "Vancouver Coastal", "value": "Vancouver Coastal"},
-                    {"label": "Fraser", "value": "Fraser"},
-                    {"label": "Vancouver Island", "value": "Vancouver Island"}],
-                        multi=True,
-                        placeholder="Select health authorities",
-                ),
-                dcc.RadioItems(className="plot-comparator", id="plot-comparator",
-                    options=[
-                        {'label': 'Age', 'value': 'age'},
-                        {'label': 'Service Type', 'value': 'service_type'},
-                    ],
-                ),
-                dcc.Dropdown(className="plot-basis", id="plot-basis", 
-                    multi=False,
-                ),
+                html.H1(className="title", children=["BAR CHART"]),
                 html.Br(),
 
                 # stacked bar
                 dcc.Graph(id='grouped_bar'),
                 html.Div(id="test"),
-                dcc.Dropdown(className="slct-ctas", id="slct-ctas", 
-                multi=False,
-                value="1-3",
-                ),
-
+                html.H1("Cost of Care Summary"),
+                html.Br(),
                 # NEW TABLE
                 dash_table.DataTable(
                     id="table",
@@ -165,50 +98,6 @@ app.layout = html.Div(className="main-content", children=[
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
 # each callback has inputs and outputs, along with a function with arguments that realte to each input
-
-
-
-
-# costs table entries
-@app.callback(
-    Output("datatable", "data"), 
-    [Input(component_id='slct-service-type', component_property='value'),
-     Input(component_id='slct_ha', component_property='value'),
-     Input(component_id='slct_age', component_property='value'),
-     Input(component_id='slct-ctas', component_property='value')
-     ]
-)
-def update_table(service_slctd, ha_slctd, age_slctd, ctas_slctd):
-
-    dff = df2.copy() #always make a copy of df
-    dff =dff[["service_type", "health_authority", "ctas_admit", "age", "Total", "Lost Productivity", "Informal Caregiver","Out of Pocket"]]
-
-    # filter by service type drop down
-    dff = dff[dff["service_type"] == service_slctd]
-
-    # filter by health authority dropdown, non specific for virtual care
-    if (service_slctd != "virtual") :
-        dff = dff[dff["health_authority"] == ha_slctd]
-
-    # filter by age category dropdown
-    dff = dff[dff["age"] == age_slctd]
-    dff = dff[["Total", "Lost Productivity", "Informal Caregiver","Out of Pocket","ctas_admit"]]
-    dff = dff.transpose()
-    if (service_slctd == "emergency"):
-        index = dff.loc["ctas_admit"].tolist().index(ctas_slctd)
-    else:
-        index = 0
-
-    values = ["Total", "Lost Productivity", "Informal Caregiver","Out of Pocket"]
-    cost_summary = pd.DataFrame({'Cost Category': values, "Amount": dff.iloc[:4,index].values})
-
-    # move total sum to end of df
-    first_row = cost_summary.head(1)
-    cost_summary = cost_summary.iloc[1:]
-    cost_summary = pd.concat([cost_summary, first_row], ignore_index=True)
-
-    return cost_summary.to_dict('records')
-
 
 # cascading plot dropdown
 @app.callback(
@@ -313,24 +202,6 @@ def handle_hover(click_data):
     else:
         return "No hover data"
 
-# cascading CTAS dropdown
-@app.callback(
-    Output('slct-ctas', 'options'),
-    [Input("grouped_bar", "clickData")])
-def update_dropdown(click_data):
-
-    if click_data is not None:
-        # Access relevant information from hover_data and perform desired actions
-        data = click_data["points"][0]["customdata"]
-        selected_service = data['service type']
-
-    if (selected_service == "emergency"):
-        options=[{"label": "1-3", "value": "1-3"},
-                {"label": "4-5", "value": "4-5"},]
-    else:
-        options=[{"label": "N/A", "value": ""}]
-    return options
-
 # costs table entries
 @app.callback(
     Output("table", "data"), 
@@ -366,14 +237,31 @@ def graph_update_table(click_data):
 
         values = ["Total", "Lost Productivity", "Informal Caregiver","Out of Pocket"]
         cost_summary = pd.DataFrame({'Cost Category': values, "Amount": dff.iloc[:4,index].values})
-        if (service_slctd == "emergency"):
-            additional_rows_df = pd.DataFrame({'Cost Category': values, "Amount": dff.iloc[:4,index2].values})
-            cost_summary = pd.concat([cost_summary, additional_rows_df], ignore_index=True)
 
-        # move total sum to end of df
+         # move total sum to end of df
         first_row = cost_summary.head(1)
         cost_summary = cost_summary.iloc[1:]
         cost_summary = pd.concat([cost_summary, first_row], ignore_index=True)
+
+        if (service_slctd == "emergency"):
+            cost_summary['CTAS Level'] = ['1-3', '1-3', '1-3', '1-3']
+
+            additional_rows_df = pd.DataFrame({'Cost Category': values, "Amount": dff.iloc[:4, index2].values})
+            first_row = additional_rows_df.head(1)
+            additional_rows_df = additional_rows_df.iloc[1:]
+            additional_rows_df = pd.concat([additional_rows_df, first_row], ignore_index=True)
+            additional_rows_df['CTAS Level'] = ['4-5', '4-5', '4-5', '4-5']
+
+            # Concatenate the additional rows with the cost_summary DataFrame
+            cost_summary = pd.concat([cost_summary, additional_rows_df], ignore_index=True)
+
+            # Create a multi-index by combining 'Category' and 'Cost Category'
+            cost_summary.set_index(['CTAS Level', 'Cost Category'], inplace=True)
+            cost_summary.reset_index(inplace=True)
+
+       
+
+        
 
         return cost_summary.to_dict('records')
     else:
