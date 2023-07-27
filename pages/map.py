c0c60435ecb08959a78a7ef50b882625b9d091cb
jjessieshang <17jessies@gmail.com>
2023-07-27 14:25:56 -0700

    z-values

--- pages/map.py
+++ pages/map.py
@@ -12,26 +12,33 @@ dash.register_page(__name__)
 with open('data/Geospatial Data/chsa_2022_wgs.geojson') as f:
     jdata = json.load(f)
 
+df_distances = pd.read_csv("data/Geospatial Data/CHSA-to-Hosp-distances.csv", delimiter=",", encoding="utf-8", header=0)
+
 z_values = [1] * len(jdata['features'])
 
 # Create a DataFrame with 'id' and 'value' columns
-data = {'id': [feature['properties']['CHSA_Name'] for feature in jdata['features']],
-        'value': z_values}
+data = {'CHSA_Name': [feature['properties']['CHSA_Name'] for feature in jdata['features']]}
+
 gdf = pd.DataFrame(data)
+merged_df = pd.merge(df_distances[['CHSA_Name', 'Street distance (km)']], gdf[['CHSA_Name']], on='CHSA_Name', how='right')
+
+print(merged_df.head())
 
 # Plot the choropleth map
-fig7 = px.choropleth_mapbox(gdf, geojson=jdata,
+fig7 = px.choropleth_mapbox(merged_df, geojson=jdata,
                            featureidkey='properties.CHSA_Name',
-                           locations='id',
-                           color='value',
+                           locations='CHSA_Name',
+                           color='Street distance (km)',
                            mapbox_style='carto-positron',
                            center={"lat": 53.7267, "lon": -127.6476},  # Set the center of the map
                            zoom=4,  # Set the initial zoom level
-                           height=800
+                           height=800,
+                           color_continuous_scale='ice',
                            )
 
-fig7.update_layout(title_text='Choropleth Map with z=1 for All Features',
+fig7.update_layout(title_text='Choropleth Map with z=Street Distance (km)',
                   title_x=0.5,
+                  
                   coloraxis_reversescale=True,
                   )
 # ------------------------------------------------------------------------------
