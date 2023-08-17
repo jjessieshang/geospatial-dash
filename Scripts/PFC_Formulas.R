0fe60aa36a208ad5917fc5dcfa20bc2a07c06ca8
GrahamMP <131700015+GrahamMP@users.noreply.github.com>
2023-08-17 09:52:35 -0700

    Updated to add hosp and virtual
    Added functions to calculate hospitalization and virtual visit unit costs

--- Scripts/PFC_Formulas.R
+++ Scripts/PFC_Formulas.R
@@ -10,6 +10,13 @@ car_cost_per_km <- 0.48
 ed_los_CTAS_III <- 3.9
 ed_los_CTAS_V <- 2.7
 
+hosp_los <- 53.6
+caregiver_0_14_hosp <- 0.75
+caregiver_15_hosp <- 0.25
+
+data_usage <- 1.25
+virtual_wait <- 0.27
+virtual_appt <- 0.35
 
 # Family Medicine
 
@@ -107,3 +114,71 @@ PFC.ed <- function (wage, distance, duration, age, meal, parking, acuity) {
   
 }
 
+# Hospitalizations
+
+lp.hosp <- function (wage, duration, age) {
+  if (age >= 0 & age <= 14) {
+    lp <- 0  
+  } 
+  else if (age >= 15 & age <= 64){
+    lp <- (hosp_los + duration)*wage
+  }
+  else {
+    lp <- 0
+  }
+  
+}
+
+ic.hosp <- function (wage, duration, age) {
+  if (age >= 0 & age <= 14) {
+    ic <- (hosp_los + duration)*wage*caregiver_0_14_hosp  
+  } 
+  else if (age >= 15 & age <= 64){
+    ic <- (hosp_los + duration)*wage*caregiver_15_hosp
+  }
+  else {
+    ic <- (hosp_los + duration)*wage*caregiver_15_hosp
+  }
+}
+
+oop.hosp <- function(distance, meal, accommodation, parking) {
+  (distance*car_cost_per_km) + meal + accommodation + parking
+}
+
+PFC.hosp <- function (wage, distance, duration, age, meal, accommodation, parking) {
+  lp.hosp(wage, duration, age) + ic.hosp(wage, duration, age) + oop.hosp(distance, meal, accommodation, parking)
+  
+}
+
+# Virtual Visits
+
+lp.virtual <- function (wage, age) {
+  if (age >= 0 & age <= 14) {
+    lp <- 0  
+  } 
+  else if (age >= 15 & age <= 64){
+    lp <- (virtual_wait + virtual_appt)*wage
+  }
+  else {
+    lp <- 0
+  }
+}
+
+ic.virtual <- function (wage, age) {
+  if (age >= 0 & age <= 14) {
+    ic <- (virtual_wait + virtual_appt)*wage*caregiver_0_14  
+  } 
+  else if (age >= 15 & age <= 64){
+    ic <- (virtual_wait + virtual_appt)*wage*caregiver_15
+  }
+  else {
+    ic <- (virtual_wait + virtual_appt)*wage*caregiver_15
+  }
+}
+
+oop.virtual <- data_usage
+
+PFC.virtual <- function (wage, age) {
+  lp.virtual(wage, age) + ic.virtual (wage, age) + oop.virtual
+  
+}
